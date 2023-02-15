b25ca10c3d1e956560d0cc4fac1aacf78607bb36
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 15:54:50 -0600

    convertir tipo de dato

--- 1_variables_tipo/tipo.py
+++ 1_variables_tipo/tipo.py
@@ -25,3 +25,13 @@ print(type(tupla))
 print(type(json))
 print(type(rango))
 print(type(dato_byte))
+
+#convertir tipo de datos
+texto = "Hola soy un texto"
+numerico = str(102)
+print(texto + " " + numerico)
+
+num1 = int(numerico)
+num2 = float(numerico)
+print(type(num1))
+print(type(num2))
