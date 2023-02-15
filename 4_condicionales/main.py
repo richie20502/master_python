d8e12dc3ac682c50949340b6a08c8e24044631c7
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 17:21:08 -0600

    operadores de comparacion

--- 4_condicionales/main.py
+++ 4_condicionales/main.py
@@ -1,7 +1,24 @@
-#primer ejecrcio
+#primer ejecrcio condicional if
 numero = 10
 
 if numero == 10:
     print(f"El valor de la variable es {numero}")
 else:
     print("no es igual a 10")
+
+#operadores de comparacion
+"""
+== igual 
+!= diferente
+< menor que 
+> mayor que
+<= menor igual que 
+>= mayor igual que
+"""
+year = int(input("dime el año en el que estamos ?"))
+
+if year >= 2023:
+    print("estamos en 2023 en ")
+else 
+    print("no estas al año")
+
