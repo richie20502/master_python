d289a9406cc2d9b416867fbfd4e54c3548e7f515
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 16:14:25 -0600

    operaciones aritmeticas

---
+++ 2_operadores/matematicos.py
@@ -0,0 +1,13 @@
+num1 = 10
+num2 = 30
+
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+resto = num1 % num2
+
+print(f"la suma es: {num1 + num2}")
+print(f"la resta es: {resta}")
+print(f"la multiplicacion: {multiplicacion}")
+print(f"la division: {division}")
+print(f"el resto: {resto}")
