7f5f666e96abc64e4187e49bf411a19ac5a63966
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 16:05:12 -0600

    cpombinacion de algunos string

---
+++ 1_variables_tipo/string_diferentes.py
@@ -0,0 +1,14 @@
+t1 = "'Master'"
+t2 = "en \"PYTHON\""
+
+texto_u = t1 + " " + t2
+print(texto_u)
+
+texto_u = t1 + " \n " + t2
+print(texto_u)
+
+texto_u = t1 + " \t " + t2
+print(texto_u)
+
+texto_u = t1 + " \r " + t2
+print(texto_u)
