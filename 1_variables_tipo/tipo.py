24c5ba5cf037930226f8f27176bdcf287e3cf786
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 15:49:37 -0600

    tipos de datos

---
+++ 1_variables_tipo/tipo.py
@@ -0,0 +1,27 @@
+#tipo de dato none
+none = None
+string = "hola mundo"
+integer = 10
+double = 1.90
+boolean = True
+list = [1,2,3,4,5,6]
+listS = [1,"hola",10]
+tupla = ("esta","es","una","tupla")
+json = { "a" : "esta es A", "b": "estas es B"}
+rango = range(10)
+dato_byte = b"byte"
+
+print(none)
+
+#tipo de dato 
+print(type(none))
+print(type(string))
+print(type(integer))
+print(type(double))
+print(type(boolean))
+print(type(list))
+print(type(listS))
+print(type(tupla))
+print(type(json))
+print(type(rango))
+print(type(dato_byte))
