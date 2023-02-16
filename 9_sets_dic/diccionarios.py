8634317f7ac83f881f3399599a8736aae0b026da
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-16 14:58:01 -0600

    diccionarios dentro de liustas

--- 9_sets_dic/diccionarios.py
+++ 9_sets_dic/diccionarios.py
@@ -5,3 +5,24 @@ mi = {
 }
 print(mi)
 print(type(mi))
+
+nombres = [
+    {
+    "nombre" : "nombre",
+    "apellido" : "apellido",
+    "sitio" : "sitio"
+    },
+    {
+    "nombre" : "nombre2",
+    "apellido" : "apellido2",
+    "sitio" : "sitio2"
+    },
+    {
+    "nombre" : "nombre3",
+    "apellido" : "apellido3",
+    "sitio" : "sitio3"
+    }
+]
+
+print(nombres)
+print(nombres[0]['nombre'])
