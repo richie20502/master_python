35efb6747efba08aa660c338fd4f88ff6abaa480
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 18:58:57 -0600

    ejercicio ciclo for tabla multiplicar

--- 5_bucles/for.py
+++ 5_bucles/for.py
@@ -6,3 +6,10 @@ for contador in range(0,5):
     print("voy por el "+ str(contador))
     resultado = resultado + contador
 print(f"el resultado es: {resultado}")
+
+#ejemplo tabla de multiplicar
+
+tabla = 2
+
+for tabla_m in range (0, 10):
+    print(f"{tabla} x {tabla_m} = {tabla*tabla_m}")
