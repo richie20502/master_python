e5546c5b73f1448f55af3d25042715238aceb322
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 19:35:33 -0600

    ejercicio numero 2

---
+++ 6_ejercicios/2.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio 2. Escribir un script que nos muestre por pantalla 
+todos los numeros pares del 1 al 100
+"""
+
+contador = 1
+
+for contador in range(1, 121):
+    if contador%2 == 0:
+        print(contador)
