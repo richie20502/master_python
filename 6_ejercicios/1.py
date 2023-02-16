9ca8050fab8c7dd64306fca21a0c785e1105693e
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 19:32:28 -0600

    ejercicios, ejercicio 1

---
+++ 6_ejercicios/1.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 1:
+    - crear variables pais y continente
+    - Mostrar su valor por pantalla (imprimir)
+    - poner un comentario  diciendo el tipo de dato
+"""
+
+pais = "México"             #string
+continente = "Americano"    #string  
+year = 2023                 #integer
+
+print(f"{pais} - {continente} - {str(year)}")
