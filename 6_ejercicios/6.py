01db0133a94afaf0435da63eecd8d2eace2eb9cd
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 21:48:09 -0600

    ejercicio 6

--- 6_ejercicios/6.py
+++ 6_ejercicios/6.py
@@ -2,3 +2,9 @@
 Mostrar todas las tablas de multiplicar del 1 al 10
 y despues multiplicar del 1 al 10
 """
+
+for cabecera in range(1, 11):
+    print(f"tabla del {cabecera}")
+
+    for numero in range(1,11):
+        print(f"{numero} x {cabecera} = {numero*cabecera}")
