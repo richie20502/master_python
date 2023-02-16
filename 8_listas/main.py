e01264a0948c92731ddc4fe92b63aaff16b49e99
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-16 13:37:13 -0600

    indices en listas

--- 8_listas/main.py
+++ 8_listas/main.py
@@ -1,3 +1,4 @@
+#listas
 numero = 10
 numeros =[2,7,8,34,10]
 nombre = list(("n1", "n2", "n3", "n5"))
@@ -6,3 +7,7 @@ years = list(range(2010,2020))
 print(numero)
 print(numeros)
 print(nombre)
+
+#indices
+print(numeros[0])
+print(nombre[3])
