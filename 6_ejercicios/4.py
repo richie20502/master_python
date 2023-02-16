3ce95540d2f95b3c2ab70c7461e31865f3791376
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 19:55:16 -0600

    ejercicio numero 4

---
+++ 6_ejercicios/4.py
@@ -0,0 +1,12 @@
+"""
+Tener dos numeros y hacer todas las operaciones basicas de una
+calculadora y mostrarlo por pantalla
+"""
+
+n1= 1
+n2= 4
+
+print(f"suma = {str(n1+n2)}")
+print(f"resta = {str(n1*n2)}")
+print(f"multiplicacion = {str(n1*n2)}")
+print(f"division = {str(n1/n2)}")
