59b088d88b0abc1e9444776d46868524e2a68262
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 22:37:00 -0600

    ejemplos de funciones

---
+++ 7_funciones/main.py
@@ -0,0 +1,12 @@
+# ejemplo 1
+def muestraNombre():
+    print("nombre 1")
+    print("nombre 2")
+    print("nombre 3")
+    print("nombre 4")
+    print("nombre 5")
+    print("nombre 6")
+
+
+#invocar funcion
+muestraNombre()
