eba0e0db01229ac874415dd17f0e130c2b09aa82
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-15 19:17:53 -0600

    ejercicio de tabla de multiplicar

--- 5_bucles/while.py
+++ 5_bucles/while.py
@@ -4,3 +4,9 @@ contador = 1
 while contador <= 100:
     print(f"el contador inicia en: {contador}")
     contador += 1
+
+tabla = 1
+contador = 1
+while contador <= 10:
+    print(f"{tabla} x {contador} = {tabla*contador}")
+    contador +=1
