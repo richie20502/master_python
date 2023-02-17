c4ebf065886c0e56962eb1fd5a323137a319e9da
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-16 23:34:00 -0600

    modulo fecha

--- 11_modulos_paquetes/main.py
+++ 11_modulos_paquetes/main.py
@@ -9,3 +9,16 @@ print(cad)
 from mimodulo import holaMundo
 
 print(holaMundo("nononon"))
+
+import datetime
+print(datetime.date.today())
+fecha_completa = datetime.datetime.now()
+print(fecha_completa)
+print(fecha_completa.year)
+print(fecha_completa.month)
+print(fecha_completa.day)
+
+fecha_personalizada = fecha_completa.strftime("%d/%m/%Y, %H:%M:%S")
+print("fecha personalizada", fecha_personalizada)
+
+print(datetime.datetime.now().timestamp())
