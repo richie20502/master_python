47994c99127a984d32c983558643c74e509d6b79
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-16 23:42:51 -0600

    modulo de matematicas

--- 11_modulos_paquetes/main.py
+++ 11_modulos_paquetes/main.py
@@ -10,6 +10,7 @@ from mimodulo import holaMundo
 
 print(holaMundo("nononon"))
 
+#modulo fecha
 import datetime
 print(datetime.date.today())
 fecha_completa = datetime.datetime.now()
@@ -22,3 +23,17 @@ fecha_personalizada = fecha_completa.strftime("%d/%m/%Y, %H:%M:%S")
 print("fecha personalizada", fecha_personalizada)
 
 print(datetime.datetime.now().timestamp())
+
+#modulo de matematicas
+
+import math
+
+print("raiz :", math.sqrt(10))
+print("pi:", float(math.pi))
+print("redondear:", math.floor(8.999))
+
+#modulo random
+
+import random
+
+print("aleatorio del 10 al 20: ", random.randint(10,20))
