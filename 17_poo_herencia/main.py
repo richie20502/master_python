f950d9b88778ea15140be76f9baae13b839e8af2
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-20 21:18:39 -0600

    herencias parte 2

--- 17_poo_herencia/main.py
+++ 17_poo_herencia/main.py
@@ -8,3 +8,12 @@ persona.setEdad("100 años")
 
 print(f"persona: {persona.getNombre()}  {persona.getApellidos()}")
 print(persona.hablar())
+print("--------------------------")
+
+informatico = clases.Informatico()
+informatico.setNombre("inform")
+informatico.setApellidos("inf apt")
+print(f"informatico: {informatico.getNombre} {informatico.getApellidos()}")
+print(informatico.getLenguajes())
+print(informatico.caminar())
+print("--------------------------")
