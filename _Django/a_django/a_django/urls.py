d41a1295fd735844e109e2fede6995fa3db6a825
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-22 18:27:32 -0600

    vistas y rutas

--- _Django/a_django/a_django/urls.py
+++ _Django/a_django/a_django/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from miapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hola/',views.hola_mundo, name="hola_mundo")
+
 ]
