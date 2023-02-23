dbf2ba7e56377bfc66112334df491145dd9a2890
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-22 18:44:40 -0600

    ejemplos de vistas

--- _Django/a_django/miapp/views.py
+++ _Django/a_django/miapp/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("""
+    html ="""
     <h1>inicio</h1> 
-    """)
+    <ul>
+    """
+    year=2023
+    while year <= 2050:
+        html +="<li>"
+        html += str(year)    
+        html +="</li>"
+        year += 1
+    
+    html += "</ul>"
+    return HttpResponse(html)
 
 def hola_mundo(request):
     return HttpResponse("hola mundo Django")
