5c46f1c5fff8a705e3203891e72a7b637269ccca
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-27 15:10:50 -0600

    genero el modelo de categorias

--- _Django/ProyectoDjango/blog/models.py
+++ _Django/ProyectoDjango/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    description = models.CharField(max_length=255, verbose_name='Descripcion')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+    
+    def __str__(self):
+        return self.name
