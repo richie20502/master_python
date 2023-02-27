d3e2de4694962835ce4f979722db6e6fa9e3f1f7
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-27 17:36:14 -0600

    creacion de modelo articulos y relcion de modelos

--- _Django/ProyectoDjango/blog/models.py
+++ _Django/ProyectoDjango/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nombre")
     description = models.CharField(max_length=255, verbose_name='Descripcion')
-    create_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
 
     class Meta:
         verbose_name = 'Categoria'
@@ -13,3 +14,20 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=150, verbose_name="titulo")
+    content = RichTextField(verbose_name="contenido")
+    image = models.ImageField(default='null', verbose_name='Imagenes')
+    public = models.BooleanField(verbose_name='Publicado')
+    user = models.ForeignKey(User,verbose_name="Usuario", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category,verbose_name="Categorias", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Actualizado")
+
+    class Meta:
+        verbose_name = 'Articulo'
+        verbose_name_plural = 'Articulos'
+    
+    def __str__(self):
+        return self.title
