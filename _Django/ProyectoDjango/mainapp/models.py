4f4046003b5720ca1a1b5ba8763ddb9c54682d22
TheRichie <richie.lugo.recillas.1990@gmail.com>
2023-02-26 22:55:43 -0600

    configuracion del panel administrativo

--- _Django/ProyectoDjango/mainapp/models.py
+++ _Django/ProyectoDjango/mainapp/models.py
@@ -1,19 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Page(models.Model):
-    title = models.CharField(max_length=50, verbose_name="titulo")
-    content = models.TextField(verbose_name='Contenido')
-    slug = models.CharField(unique = True, max_length=150, verbose_name="URL amigable")
-    visible = models.BooleanField(verbose_name="Visible?") 
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado:")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Actualizado:")
-
-    class Meta:
-        verbose_name = "Página"
-        verbose_name_plural = "Páginas"
-
-    def __str__(self):
-        return self.title
-
--- _Django/ProyectoDjango/pages/admin.py
+++ _Django/ProyectoDjango/pages/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Page
 # Register your models here.
+
+
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at','updated_at')
+
+admin.site.register(Page,PageAdmin)
+
+title = "Proyecto Page"
+subtitle = "Gestion"
+
+admin.site.site_header = title
+admin.site.site_title = title
+admin.site.index_title = subtitle
